5be38fbb10c97030e3db034ab0aac3d54a7c1e93
Jitesh Raghav <93904444+Jitesh-Raghav@users.noreply.github.com>
2023-09-12 20:55:20 +0530

    Time: 20 ms (50.38%), Space: 49.6 MB (11.88%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int sum = 0, count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int j=0;j<nums.length;j++){
+            sum += nums[j];
+            if(map.containsKey(sum -goal)){
+                count += map.get(sum-goal);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
