f5f9a6ae515a9b50604c0e0eb85a0d9c32de9211
Jitesh Raghav <93904444+Jitesh-Raghav@users.noreply.github.com>
2023-10-23 00:29:50 +0530

    Time: 1 ms (64.99%), Space: 40.1 MB (94.09%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minAddToMakeValid(String S) {
+         int res = 0;
+       Stack<Character> stack = new Stack<>();
+       for(char c : S.toCharArray()){
+           if(c == '('){
+               stack.push(c);
+           }
+           else{
+               if(stack.empty())
+                   res++;
+               else
+                   stack.pop();
+           }
+       }
+       return res+stack.size();
+    }
+}
+//SEE THIS CODE WORKING WITH EXAMPLE IN CHATGPT...
